46356c9f3fa11751d7744103f920c28d80d98c24
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-06-24 13:07:08 +0100

    description effect

--- js/oaktree.js
+++ js/oaktree.js
@@ -15,12 +15,39 @@ function showSmartyPants(){
 	smpants.innerHTML="hello";
 }
 
+function visElemEvent(event,classname,vis){
+	event.target.getElementsByClassName(classname)[0].style.visibility=vis;
+}
+
+function showDescription(event){
+	visElemEvent(event,"project-title","hidden");
+	visElemEvent(event,"project-description","visible");
+}
+
+function hideDescription(event){
+	visElemEvent(event,"project-description","hidden");
+	visElemEvent(event,"project-title","visible");
+
+}
+
 
 
 
 
 //=============== initialize
 window.onload=function(){
+	//Hides project descriptions
+	var descr = document.getElementsByClassName("project-description");
+	for (var i =0; i<descr.length;i++){
+		descr[i].style.visibility="hidden";
+	}
+	var projects = document.getElementsByClassName("project");
+	for (var i =0; i<projects.length;i++){
+		projects[i].addEventListener("mouseenter", showDescription);
+		projects[i].addEventListener("mouseleave", hideDescription);
+
+	}
+
 	//light.addEventListener("click",lightSwitch);
 	//smpants.addEventListener("mouseover", showSmartyPants);
 
